fa36288f813b63b9278a7cbcb65b35c60cda7710
Diegooooo19483 <damelo35@ucatolica.edu.co>
2025-10-30 07:49:21 -0500

    Config Final

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings  # ✅ CAMBIO IMPORTANTE
+from pydantic_settings import BaseSettings
 
-# Cargar variables del archivo .env
+
 load_dotenv()
 
 class Settings(BaseSettings):
